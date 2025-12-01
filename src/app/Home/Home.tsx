040d0f280d40e50bb3e567514ecd1e7b4dff9b64
Andy Braren <abraren@redhat.com>
2025-12-01 01:27:42 -0500

    Revert text change

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -9,7 +9,10 @@ const Home: React.FunctionComponent = () => (
   <PageSection>
     <Content component={ContentVariants.h1}>Home</Content>
     <Content component={ContentVariants.p}>
-      Welcome to the RHOAI Dashboard User Research Prototype. This prototype is for user research purposes only and does not represent any commitment on the part of Red Hat.
+      Welcome to the RHOAI Dashboard User Research Prototype.
+    </Content>
+    <Content component={ContentVariants.p}>
+      NOTE: This prototype is for user research purposes only and does not represent any commitment on the part of Red Hat.
     </Content>
   </PageSection>
 );
